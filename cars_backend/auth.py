205034a15a3c8adcb40c4e4817d3ade910e0f9ce
Bassel Ballout El Mawla <balloutelb@coventry.ac.uk>
2022-02-02 20:17:02 +0000

    Merged both backends

---
+++ cars_backend/auth.py
@@ -0,0 +1,109 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from cars_backend.db import get_db
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == 'POST':
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        db = get_db()
+        error = None
+
+        if not first_name:
+            error = "First name is required."
+        elif not last_name:
+            error = "Last name is required."
+        elif not email:
+            error = "Email is required."
+        elif not password:
+            error = "Password is required."
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (first_name, last_name, email, hashed_password, salt) "
+                    "VALUES (?, ?, ?, ?, 'test')",
+                    (first_name, last_name, email, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {first_name} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        print("Email: ", email)
+        print("Password: ", password)
+
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE email = ?', (email,)
+        ).fetchone()
+
+        print("User object: ", user)
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["hashed_password"], password):
+            error = "Incorrect password."
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["user_id"]
+            return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
+
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE user_id = ?', (user_id,)
+        ).fetchone()
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
---
+++ cars_backend/schema.sql
@@ -0,0 +1,46 @@
+BEGIN TRANSACTION;
+DROP TABLE IF EXISTS "car";
+CREATE TABLE IF NOT EXISTS "car" (
+	"car_id"	,
+	"model"	,
+	"year"	,
+	"colour"	,
+	"price"	,
+	"number_of_doors"	,
+	"number_of_seats"	,
+	"user_id"
+);
+DROP TABLE IF EXISTS "order_info";
+CREATE TABLE IF NOT EXISTS "order_info" (
+	"order_info_id"	,
+	"datetime"	,
+	"total"	,
+	"user_id"
+);
+DROP TABLE IF EXISTS "user";
+CREATE TABLE IF NOT EXISTS "user" (
+	"user_id" INTEGER PRIMARY KEY,
+	"first_name"	,
+	"last_name"	,
+	"email"	,
+	"hashed_password"	,
+	"salt"
+);
+DROP TABLE IF EXISTS "car_in_order";
+CREATE TABLE IF NOT EXISTS "car_in_order" (
+	"order_info_id"	,
+	"car_id"
+);
+DROP TABLE IF EXISTS "basket";
+CREATE TABLE IF NOT EXISTS "basket" (
+	"user_id"	,
+	"car_id"
+);
+INSERT INTO "car" ("car_id","model","year","colour","price","number_of_doors","number_of_seats","user_id") VALUES ('1','Audi G8','2018','Dark Blue','26000','4','5','1');
+INSERT INTO "car" ("car_id","model","year","colour","price","number_of_doors","number_of_seats","user_id") VALUES ('2','BMW F27','2006','White','8000','2','4','2');
+INSERT INTO "order_info" ("order_info_id","datetime","total","user_id") VALUES ('1','2022-01-26 13:45:12','8000','1');
+INSERT INTO "user" ("user_id","first_name","last_name","email","hashed_password","salt") VALUES ('1','John','Smith','johnsmith@gmail.com','abc123','321');
+INSERT INTO "user" ("user_id","first_name","last_name","email","hashed_password","salt") VALUES ('2','Bob','Marley','bobmarley@gmail.com','abc123','321');
+INSERT INTO "car_in_order" ("order_info_id","car_id") VALUES ('1','2');
+INSERT INTO "basket" ("user_id","car_id") VALUES ('2','1');
+COMMIT;
