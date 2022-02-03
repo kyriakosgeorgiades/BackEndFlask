72c91f711a1df94b4ab0d917a8ecb08504b2a759
Nathanael Brown (brownn32) <brownn32@coventry.ac.uk>
2022-02-03 18:45:06 +0000

    auth.py (new version)
    includes the login & registration hashing programs

--- cars_backend/auth.py
+++ cars_backend/auth.py
@@ -1,4 +1,4 @@
-import functools
+import functools, numpy, string
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
@@ -46,6 +46,83 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
+
+''' Hashing Program for Registration Purposes '''
+# Registration Hashing Program
+@bp.route("/registerHash", method=("GET", "Post"))
+def registerHash():
+    letters = 'abcdefghijklmnopqrstuvwxyz@.'
+
+    email = request.form["email"]
+    password = request.form["password"]
+
+    # Random Key Generation
+    upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
+    numbers = '0123456789'
+    
+    character_bank = [upperLetters, lowerLetters, numbers]
+    alphanumeric = ''.join(character_bank)
+
+    generated = []
+    for i in range(0, 32):
+        randomiser = random.choice(alphanumeric)
+        generated.append(randomiser)
+    join = ''.join(generated)
+    keyString = join
+
+    # Random Salt Generation
+    character_list = []
+    for i in range(0, 16):
+        randomiser = random.choice(letters)
+        character_list.append(randomiser)
+    salt = ''.join(character_list)
+
+    # Combine Salt & Password
+    combineSalt_password = [salt, password]
+    joinCombine1 = ''.join(combineSalt_password)
+
+    # Hashes the Password
+    order = [letters.index(char.lower()) for char in joinCombine1]
+    hashed_password = (''.join(keyString[alpha] for alpha in order))
+
+    # Commits Registration Information To DB
+    db.execute("INSERT INTO user (email, keyString, salt, hashed_password) "
+               "VALUES (?, ?, ?, ?, 'test')",
+                (email, keyString, salt, hashed_password),
+    )
+    db.commit()
+
+
+''' Hashing Program for Login Purposes '''
+@bp.route("/loginHash", method=("GET", "Post"))
+def loginHash():
+
+    ''' 'Retrieve DB Information' Needs to specifically get DB Info - Needs Update '''
+    # Retrieve DB information 
+    email = user_input_email
+    password =user_input_password
+    database_email = DB_email
+    database_hashed_password = DB_hashed_password
+    database_hash_key = DB_hash_key
+    database_salt = DB_salt
+
+    # Combines Salt & Password
+    combineSalt_password = [database_salt, password]
+    joinCombine1 = ''.join(combineSalt_password)
+
+    # Hashs the Password
+    order = [letters.index(char.lower()) for char in joinCombine1]
+    hashed_password = (''.join(database_hash_key[alpha] for alpha in order))
+
+    # Compares User Inputted Password to DB for Login
+    if hashed_password == database_hashed_password:
+        print("Login Successful", "\n")
+        return "Login Accepted"
+    if hashed_password != database_hashed_password:
+        print("Login Unsuccessful", "\n")
+        return "Login Failure"
+
 
 
 @bp.route("/login", methods=("GET", "POST"))
