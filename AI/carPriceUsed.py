9726490d5348b6b6f346fbfe133d350195763dd6
georgiadek <georgiadek@uni.coventry.ac.uk>
2022-02-11 00:56:31 +0000

    Updates to files and tests added
    I have modified and changed the code to meet code quality metrics. I have also included the unit tests for the AI models

--- AI/carPriceUsed.py
+++ AI/carPriceUsed.py
@@ -1,19 +1,21 @@
 import pickle
+import warnings
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import warnings
+
+
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import Ridge
+from sklearn.ensemble import RandomForestRegressor
 
 from sklearn.exceptions import DataConversionWarning
 
 warnings.filterwarnings("ignore", category=DataConversionWarning)
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression, Ridge, LogisticRegression
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
-from sklearn.preprocessing import OrdinalEncoder
 
 df = pd.read_csv('Car details v3.csv')
 print(df.head())
@@ -26,10 +28,10 @@ print(df.describe())
 # Transforming the data
 cars_used = df.copy(deep=True)
 df['mileage'] = df['mileage'].str.extract(r'(\d+.\d+)').astype('float')
-df['engine'] = df['engine'].str.extract('(^\d*)')
-df['max_power'] = df['max_power'].str.extract('(^\d*)')
+df['engine'] = df['engine'].str.extract('(^\\d*)')
+df['max_power'] = df['max_power'].str.extract('(^\\d*)')
 # Torque Extraction
-df['torque'] = df['torque'].str.extract('(^\d*)')
+df['torque'] = df['torque'].str.extract('(^\\d*)')
 df['torque'] = df['torque'].fillna(150)
 df['torque'] = df['torque'].astype(int)
 # if it's in kgm, change it to N.m by multiplying by g= 9.8
@@ -58,14 +60,22 @@ plt.show()
 print(df.head())
 
 # Training and testing the model
-x = df.drop(['selling_price','name'], axis=1)
+x = df.drop(['selling_price', 'name'], axis=1)
 y = df[['selling_price']]
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=25)
-
+X_train, X_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=25)
+print("TRAINING")
+print(X_train.to_string())
 lin_reg = Ridge()
 lin_reg.fit(X_train, y_train)
 y_pred = lin_reg.predict(X_test)
-print('R2 Score for Linear Regression on test data: {}'.format(np.round(r2_score(y_test, y_pred), 3)))
+print(
+    'R2 Score for Linear Regression on test data: {}'.format(
+        np.round(
+            r2_score(
+                y_test,
+                y_pred),
+            3)))
 
 plt.scatter(y_test, y_pred)
 plt.show()
@@ -80,8 +90,14 @@ random_grid = {'n_estimators': n_estimators,
                'bootstrap': bootstrap}
 
 rf = RandomForestRegressor()
-rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid, n_iter=20, cv=3, verbose=2,
-                               random_state=42, n_jobs=-1)
+rf_random = RandomizedSearchCV(
+    estimator=rf,
+    param_distributions=random_grid,
+    n_iter=20,
+    cv=3,
+    verbose=2,
+    random_state=42,
+    n_jobs=-1)
 # Fit the random search model
 rf_random.fit(X_train, y_train)
 print(rf_random.best_params_, rf_random.best_score_)
@@ -89,18 +105,27 @@ print(rf_random.best_params_, rf_random.best_score_)
 rf = RandomForestRegressor(**rf_random.best_params_)
 rf.fit(X_train.values, y_train.values)
 
-scores = cross_val_score(rf, X_train.values, y_train.values, cv=5, scoring='r2')
+scores = cross_val_score(
+    rf,
+    X_train.values,
+    y_train.values,
+    cv=5,
+    scoring='r2')
 print('R2 Score CV for RandomForest = {}'.format(np.round(scores.mean(), 2)))
 
 y_pred = rf.predict(X_train.values)
-print('RandomForestRegressor results on train data: R2 Score: {}  - MSE Score: {}'.format(r2_score(y_train, y_pred),
-                                                                                          mean_squared_error(y_train,
-                                                                                                             y_pred)))
+print(
+    'RandomForestRegressor results on train data: R2 Score: {}  - MSE Score: {}'.format(
+        r2_score(
+            y_train, y_pred), mean_squared_error(
+            y_train, y_pred)))
 
 y_pred = rf.predict(X_test.values)
-print('RandomForestRegressor results on train data: R2 Score: {}  - MSE Score: {}'.format(r2_score(y_test, y_pred),
-                                                                                          mean_squared_error(y_test,
-                                                                                                             y_pred)))
+print(
+    'RandomForestRegressor results on train data: R2 Score: {}  - MSE Score: {}'.format(
+        r2_score(
+            y_test, y_pred), mean_squared_error(
+            y_test, y_pred)))
 plt.figure(figsize=(12, 8))
 plt.scatter(y_test, y_pred)
 plt.show()
@@ -108,21 +133,32 @@ print("I AM PRICE OF FIRST TEST")
 print(y_test)
 print(y_pred[2])
 
-filename = "../user_car_perict.sav"
-pickle.dump(rf, open(filename, 'wb'))
+FILENAME = "../user_car_perict.sav"
+pickle.dump(rf, open(FILENAME, 'wb'))
 
 print(X_test.info())
 print(X_test['seller_type'].unique())
 print(X_test)
-random_test = {"name": 123.0, "year": 2018, "km_driven": 236598, "fuel": 3.0, "seller_type": 1.0, "transmisssion": 1.0,
-               "owner": 2.0, "mileage": 15.00, "engine": 1061, "max_power": 152, "torque": 88.0, "seats": 5.0}
+random_test = {
+    "name": 123.0,
+    "year": 2018,
+    "km_driven": 236598,
+    "fuel": 3.0,
+    "seller_type": 1.0,
+    "transmisssion": 1.0,
+    "owner": 2.0,
+    "mileage": 15.00,
+    "engine": 1061,
+    "max_power": 152,
+    "torque": 88.0,
+    "seats": 5.0}
 print(random_test)
-random_test = [123.0,  2018,  236598, 3.0, 1.0,  1.0,
-                2.0,  15.00,  1061,  152,  88.0,  5.0]
-random_test = [2015,  25000, 3.0, 1.0,  1.0,
-                0.0,  17.01,  1591,  121,  154.0,  5.0]
+random_test = [123.0, 2018, 236598, 3.0, 1.0, 1.0,
+               2.0, 15.00, 1061, 152, 88.0, 5.0]
+random_test = [2015, 25000, 3.0, 1.0, 1.0,
+               0.0, 17.01, 1591, 121, 154.0, 5.0]
 # random_test = np.reshape(random_test, (1,-1)).T
-random_test = np.reshape(random_test, (1,-1))
+random_test = np.reshape(random_test, (1, -1))
 print(random_test)
 print(rf.predict(random_test))
 print(x.head().to_string)
--- cars_backend/api.py
+++ cars_backend/api.py
@@ -1,21 +1,23 @@
+"""
+Provides routes for AI related functionalities
+"""
 import csv
-import functools
 import os
 
 from PIL import Image
 from flask import (
-    Blueprint, flash, g, request, session, url_for, jsonify
+    Blueprint, request, jsonify
 )
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask_cors import CORS, cross_origin
+from flask_cors import cross_origin
 from cars_backend.preprocess import prepro, pic_prepro
-from cars_backend.db import get_db
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
 # Cars image recognition setup
+# Loading the classes name to a local variable
+# to be used with in the route of the car classification
 class_names = []
-with open('AI/names.csv', newline='') as Cars:
+with open('AI/names.csv', newline='', encoding='UTF-8') as Cars:
     cvs_read = csv.reader(Cars)
     for row in cvs_read:
         class_names.append(row[0])
@@ -24,24 +26,65 @@ with open('AI/names.csv', newline='') as Cars:
 @bp.route("/usedCarPrice", methods=["POST"])
 @cross_origin()
 def used_car_price():
+    """ Route for predicting the price of the car based on input features
+    :
+     :returns
+     (data, status_code):
+        Returning json data of the predicted price
+        The status code of the response
+    :except
+    (err, status_code):
+        Returning the error raised
+        The status code of the response
+     """
     try:
         features = request.get_json()
         print("Car features: ", features)
+        # Calling the function to preprocess the data for the prediction
         price = prepro(features)
         print("Predicted price: ", price)
         data = {'Price': price}
-        return jsonify(data), 200
-    except:
-        return "Missing Value", 404
+        # Creating a json data for the response
+        data = jsonify(data)
+        status_code = 200
+        return data, status_code
+    except TypeError as err:
+        print(err)
+        status_code = 500
+        return jsonify(str(err)), status_code
+    except ValueError as err:
+        status_code = 500
+        return jsonify(str(err)), status_code
 
 
 @bp.route('/searchCarAI', methods=['POST'])
 def search_car_ai():
-    picture = request.files['test']
-    img = Image.open(picture)
-    img.save("AI/Temp_pics/pics/prediction.png")
-    print(picture)
-    result = pic_prepro(img)
-    print(class_names[result])
-    os.remove("AI/Temp_pics/pics/prediction.png")
-    return class_names[result]
+    """ Route for predicting the car model and brand from an input picture
+    :
+     :returns
+     (data, status_code):
+        Returning json data of the predicted car picture
+        The status code of the response
+    :except
+    (Exception, status_code):
+        Returning the error raised
+        The status code of the response
+    """
+    try:
+        picture = request.files['file']
+        img = Image.open(picture)
+        # Storing the picture to a temporary directory
+        img.save("AI/Temp_pics/pics/prediction.png")
+        print(picture)
+        # Function call for preprocessing the picture and prediction
+        result = pic_prepro()
+        print(class_names[result])
+        # Removing the picture from the temporary directory
+        os.remove("AI/Temp_pics/pics/prediction.png")
+        status_code = 200
+        data = class_names[result]
+        return data, status_code
+    except IOError as err:
+        print(err)
+        status_code = 500
+        return jsonify(str(err)), status_code
