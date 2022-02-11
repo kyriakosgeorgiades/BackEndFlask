6335b0b7cf3d004e8c54f132f97bd39a781fae29
georgiadek <georgiadek@uni.coventry.ac.uk>
2022-02-11 03:05:27 +0000

    New route of Car ID
    The route is created that is getting the ID of the car based on the brand and model of the car. This is to help the functionality of the search.

--- cars_backend/car.py
+++ cars_backend/car.py
@@ -41,6 +41,41 @@ def get_all_cars():
             return jsonify({"message": "System error", "status": 401})
 
 
+@bp.route("/getID", methods=["POST"])
+@cross_origin()
+def get_name():
+    """ Route for predicting getting the ID of a car by brand and name
+    :
+     :returns
+     (car["car_id"], status):
+        Returning json data of ID of the car
+        The status code of the response
+    :except
+    (Exception, status_code):
+        Returning the error raised
+        The status code of the response
+    """
+    db = get_db()
+    try:
+        brand_name = request.form['name']
+        # Breaking down the string
+        brand_name = brand_name.split(" ", 1)
+        # Brand of the car broken down from the string
+        brand = brand_name[0]
+        size = len(brand_name[1])
+        # Model broken down from the string
+        model = brand_name[1][:size - 5]
+        # Query to fecth the ID of the car to be returned
+        car = db.execute('SELECT car_id FROM car WHERE brand = ? AND model = ?', (brand, model)).fetchone()
+
+        return jsonify({"ID": car["car_id"], "status": 200})
+
+    except IndexError:
+        return jsonify({"message": "System error", "status": 401})
+    except db.IntegrityError:
+        return jsonify({"message": "System error", "status": 401})
+
+
 @bp.route("/view/<car_id>", methods=["GET"])
 @cross_origin()
 def view_car(car_id):
@@ -82,7 +117,8 @@ def find_similar():
             result = []
 
             # economical alternatives are: smaller engine, higher economy, similar year, similar price
-            cars = db.execute('SELECT * FROM car WHERE engine < ? AND fuel_consumption > ? AND year >= ? AND price < ?', (engine, economy, year-2, price+5000)).fetchall()
+            cars = db.execute('SELECT * FROM car WHERE engine < ? AND fuel_consumption > ? AND year >= ? AND price < ?',
+                              (engine, economy, year - 2, price + 5000)).fetchall()
 
             for row in cars:
                 row = dict(row)
@@ -92,7 +128,8 @@ def find_similar():
                 result.append(row)
 
             # value alternatives are: smaller price, lower mileage, similar year
-            cars = db.execute('SELECT * FROM car WHERE price < ? AND km_driven < ? AND year >= ?', (price, mileage, year-1)).fetchall()
+            cars = db.execute('SELECT * FROM car WHERE price < ? AND km_driven < ? AND year >= ?',
+                              (price, mileage, year - 1)).fetchall()
 
             for row in cars:
                 row = dict(row)
@@ -102,7 +139,8 @@ def find_similar():
                 result.append(row)
 
             # performance alternatives are: higher engine, higher bhp, similar year, similar price
-            cars = db.execute('SELECT * FROM car WHERE engine > ? AND max_power > ? AND year >= ? AND price < ?', (engine, bhp, year-2, price+5000)).fetchall()
+            cars = db.execute('SELECT * FROM car WHERE engine > ? AND max_power > ? AND year >= ? AND price < ?',
+                              (engine, bhp, year - 2, price + 5000)).fetchall()
 
             for row in cars:
                 row = dict(row)
@@ -112,7 +150,8 @@ def find_similar():
                 result.append(row)
 
             # similar alternatives are: same brand, lower price, similar year
-            cars = db.execute('SELECT * FROM car WHERE brand == ? AND price < ? AND year >= ?', (brand, price, year-2)).fetchall()
+            cars = db.execute('SELECT * FROM car WHERE brand == ? AND price < ? AND year >= ?',
+                              (brand, price, year - 2)).fetchall()
 
             for row in cars:
                 row = dict(row)
